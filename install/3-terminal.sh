7ac147ba26dbf6231bf437ac7c4fa7c89211e81a
Trevor <673838+trevorrjohn@users.noreply.github.com>
2025-07-16 06:26:17 -0400

    Update 3-terminal.sh

--- install/3-terminal.sh
+++ install/3-terminal.sh
@@ -1,6 +1,6 @@
 yay -S --noconfirm --needed \
   wget curl unzip inetutils impala \
-  fd eza fzf ripgrep zoxide bat \
+  fd fzf ripgrep bat \
   wl-clipboard fastfetch btop \
   man tldr less whois plocate bash-completion \
   alacritty
